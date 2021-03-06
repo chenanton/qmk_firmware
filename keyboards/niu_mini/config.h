2ffeae6be44c778ff331cc7ae1fef1b113644950
Anton Chen <contact@antonchen.ca>
2021-03-06 01:38:40 -0700

    Add personal keymap and update config

--- keyboards/niu_mini/config.h
+++ keyboards/niu_mini/config.h
@@ -19,7 +19,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include "config_common.h"
 
-#define TAPPING_TERM 175
+#define TAPPING_TERM 200
 #define IGNORE_MOD_TAP_INTERRUPT
 
 /* USB Device descriptor parameter */
--- keyboards/niu_mini/keymaps/anton/keymap.c
+++ keyboards/niu_mini/keymaps/anton/keymap.c
@@ -1,16 +1,20 @@
 #include QMK_KEYBOARD_H
 
 // Left-hand home row mods
-#define HOME_A LGUI_T(KC_A)
-#define HOME_O LALT_T(KC_O)
-#define HOME_E LSFT_T(KC_E)
-#define HOME_U LCTL_T(KC_U)
+#define HOME_SC LGUI_T(KC_SCLN)
+#define HOME_Q LALT_T(KC_Q)
+#define HOME_J LSFT_T(KC_J)
+#define HOME_K LCTL_T(KC_K)
 
 // Right-hand home row mods
-#define HOME_H RCTL_T(KC_H)
-#define HOME_T RSFT_T(KC_T)
-#define HOME_N LALT_T(KC_N)
-#define HOME_S RGUI_T(KC_S)
+#define HOME_M RCTL_T(KC_M)
+#define HOME_W RSFT_T(KC_W)
+#define HOME_V LALT_T(KC_V)
+#define HOME_Z RGUI_T(KC_Z)
+
+#define MOD_SPC LT(2, KC_SPC)
+#define TMUX_T LCTL(KC_B)
+#define STK_SFT OSM(MOD_LSFT)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
@@ -26,10 +30,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * `-----------------------------------------------------------------------------------'
      */
     [0] = LAYOUT_ortho_4x12(
-        KC_CAPS, KC_QUOT, KC_COMM, KC_DOT,  KC_P,    KC_Y,    KC_F,   KC_G,    KC_C,    KC_R,    KC_L,    KC_ENT,
-        KC_TAB,  HOME_A,  HOME_O,  HOME_E,  HOME_U,  KC_I,    KC_D,   HOME_H,  HOME_T,  HOME_N,  HOME_S,  KC_MINS,
-        KC_LPRN, KC_SCLN, KC_Q,    KC_J,    KC_K,    KC_X,    KC_B,   KC_M,    KC_W,    KC_V,    KC_Z,    KC_RPRN,
-        KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_ESC,  TT(1),   KC_SPC, KC_BSPC, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
+        KC_TAB,  KC_QUOT, KC_COMM, KC_DOT,  KC_P,    KC_Y,    KC_F,    KC_G,    KC_C,    KC_R,    KC_L,    TMUX_T,
+        KC_ESC,  KC_A,    KC_O,    KC_E,    KC_U,    KC_I,    KC_D,    KC_H,    KC_T,    KC_N,    KC_S,    KC_ENT,
+        KC_BSLS, HOME_SC, HOME_Q,  HOME_J,  HOME_K,  KC_X,    KC_B,    HOME_M,  HOME_W,  HOME_V,  HOME_Z,  KC_SLSH,
+        KC_NO,   KC_NO,   KC_NO,   KC_CAPS, STK_SFT, TT(1),   MOD_SPC, KC_BSPC, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
     ),
 
     /* Layer 1: Numbers and Symbols
@@ -44,9 +48,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * `-----------------------------------------------------------------------------------'
      */
     [1] = LAYOUT_ortho_4x12(
-        _______, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_LBRC, KC_RBRC, KC_1,    KC_2,    KC_3,    KC_4,   _______,
-        _______, KC_GRV,  KC_TILD, KC_PIPE, KC_PLUS, KC_BSLS, KC_SLSH, KC_9,    KC_0,    KC_QUES, KC_EQL,  _______,
-        _______, KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LCBR, KC_RCBR, KC_5,    KC_6,    KC_7,    KC_8,    _______,
+        _______, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_LCBR, KC_RCBR, KC_1,    KC_2,    KC_3,    KC_4,    _______,
+        KC_UNDS, KC_GRV,  KC_TILD, KC_PIPE, KC_PLUS, KC_LPRN, KC_RPRN, KC_9,    KC_0,    KC_QUES, KC_EQL,  KC_MINS,
+        _______, KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LBRC, KC_RBRC, KC_5,    KC_6,    KC_7,    KC_8,    _______,
         RESET,   _______, _______, _______, KC_ESC,  KC_TRNS, KC_SPC,  KC_BSPC, _______, _______, _______, RGB_TOG
     ),
 
@@ -62,9 +66,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * `-----------------------------------------------------------------------------------'
      */
     [2] = LAYOUT_ortho_4x12(
-        _______, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______,
-        _______, RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, _______, _______, KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS,
-        _______, BL_TOGG, BL_STEP, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______, _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,    _______,
+        _______, RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, _______, _______, KC_F5,   KC_F6,   KC_F7,   KC_F8,    _______,
+        _______, BL_TOGG, BL_STEP, _______, _______, _______, _______, KC_F9,   KC_F10,  KC_F11,  KC_F12,   _______,
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
     )
 };
